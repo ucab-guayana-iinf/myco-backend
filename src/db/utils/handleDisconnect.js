ddd9bae84d19e9ce8ebc4c04e1da7e797254a267
Angel Rodríguez <angelmanuelrs@gmail.com>
2019-05-05 00:48:53 -0400

    Fix console.log

--- src/db/utils/handleDisconnect.js
+++ src/db/utils/handleDisconnect.js
@@ -1,9 +1,8 @@
 const connect = require('./connect');
-const { log } = require('../../utils');
 
 function handleDisconnect(client) {
   client.on('error', async (error) => {
-    log.message(`> Re-connecting lost MySQL connection: ${error}`);
+    console.log(`> Re-connecting lost MySQL connection: ${error}`);
 
     const connection = await connect();
 
